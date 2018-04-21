cf8f7613ad8926e5f83d2a80f27cacefab3886b8
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-04-21 13:56:28 +0200

    Scripts

---
+++ Experience Game Jam #4/Assets/Scripts/Base.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+    public int leben;
+
+    // Use this for initialization
+    void Start () {
+        leben = 5;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collision col)
+    {
+        if (col.gameObject.tag == "enemy")
+        {
+            Destroy(col.gameObject);
+            leben -= 1;
+        }
+    }
+}
---
+++ Experience Game Jam #4/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public EnemyBase enemyBase;
+    public Base Base;
+    public GameObject loseScreen;
+    public GameObject winScreen;
+
+    // Use this for initialization
+    void Start () {
+        loseScreen.SetActive(false);
+        winScreen.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Base.leben < 1) {
+            loseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if (enemyBase.enemyCount < 1) {
+            winScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+	}   
+}
