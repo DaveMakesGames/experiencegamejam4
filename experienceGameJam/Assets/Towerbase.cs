7e3b8a6a203a5aead80cf8205c27fa986093783c
DESKTOP-G7O99JD\negro <madhatsseven@gmail.com>
2018-04-21 15:18:19 +0200

    stuff

--- experienceGameJam/Assets/Towerbase.cs
+++ experienceGameJam/Assets/Towerbase.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Towerbase : MonoBehaviour
 {
-    public GameObject towerRange;
+    public GameObject shootingRange;
     public GameObject enemy;
+    public float aoeDamage = 25;
     public float enemyLife;
     
 
@@ -13,13 +14,42 @@ public class Towerbase : MonoBehaviour
     {
         if(other.gameObject.tag == "enemy")
         {
-
+            Attack();
         }
     }
 
-    public void AOE_Damage()
+    public void Attack()
     {
 
+    }
+
+    public virtual void FindRandomTarget()
+    {
+        if (shootingRange == null)
+        {
+            return;
+        }
+
+        int layer = 1 << 8;
+        float radius = shootingRange.GetComponent<SphereCollider>().radius * 2;
+        Collider[] stumpTargets = Physics.OverlapSphere(transform.position, radius, layer);
+        if (stumpTargets == null)
+        {
+            return;
+        }
+
+        var collisionsCount = stumpTargets.Length;
+        for (var i = 0; i < collisionsCount; ++i)
+        {
+            var target = stumpTargets[i].GetComponent<EnemyStats>();
+            if (target == null/* || !target.IsAlive*/)
+            {
+                continue;
+            }
+
+            target.life -= aoeDamage;
+            //StompAttack(target);
+        }
     }
 
     // Use this for initialization
