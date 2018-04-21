d319df7479e65a10dd21da0fdef2b72f39c18e48
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2018-04-21 15:11:51 +0200

    - UI

---
+++ Experience Game Jam #4/Assets/Scripts/UI/StartOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartOnClick : MonoBehaviour {
+
+    public Button yourButton;
+    public GameObject titleScreen;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(load);
+    }
+
+    public void load()
+    {
+        titleScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
